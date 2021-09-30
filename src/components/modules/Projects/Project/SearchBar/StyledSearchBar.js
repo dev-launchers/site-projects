d1aea2c30ed2a9bf85a57816c1a39e5647c3a994
Marco <35307131+MarcoB-Ec@users.noreply.github.com>
2021-09-30 11:55:39 -0500

    feat(MarcoB-Ec): Fix css and search funtionality

--- src/components/modules/Projects/Project/SearchBar/StyledSearchBar.js
+++ src/components/modules/Projects/Project/SearchBar/StyledSearchBar.js
@@ -8,18 +8,13 @@ export const SearchBox = styled.div`
 `;
 
 export const Search = styled.input`
-  cursor: text;
   max-width: ${rem("192px")};
-  height: 2rem;
-  display: inline-block;
   border: 1px solid #cfd4db;
   border-radius: 2rem;
   font-size: 1.1rem;
   line-height: 2rem;
   padding: 0 0.5rem 0 2rem;
-  outline: none;
   transition: all 0.2s ease;
-  background: #fff url(/src/icons/search.svg) 0.6rem 0.5rem no-repeat;
   background-size: 1rem;
   ::placeholder {
     color:#d6d4d5;
