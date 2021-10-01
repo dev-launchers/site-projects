eef9ff3535be17ebc950eb0dd00b16077d49a289
Gabriel Sokol <gsokol4@gmail.com>
2021-10-01 03:12:41 -0500

    calendar api call added

--- src/components/modules/Projects/Project/Sessions/Sessions.js
+++ src/components/modules/Projects/Project/Sessions/Sessions.js
@@ -6,13 +6,19 @@ import PercentageBar from "./components/PercentageBar";
 
 import { FlexBoxVerticalWrapper, Descript } from "./StyledSessions";
 
+/*
+https://github.com/dev-launchers/platform__website/blob/master/src/components/modules/UserProfile/WeeksGlance/WeeksGlance.js
+*/
+
 // import { env } from "../../../../../utils/EnvironmentVariables";
 const Sessions = ({ project, calendarId }) => {
-  console.log(project)
+  console.log(project)  
+  let key = 'AIzaSyCgXZRjXOwT6DilHJyjj5B3svz6cETj_MI'
+  console.log(calendarId)
   React.useEffect(() => {
     axios
       .get(
-        `https://www.googleapis.com/calendar/v3/users/me/calendarList/${calendarId}?key=[YOUR_API_KEY]`
+        `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${key}`
       )
       .then((res) => {
         console.log(res);
