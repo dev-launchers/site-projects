8295b41bc12ba4bde0b4545e2fbd031273c5e833
unknown <jappreet.mangat@gmail.com>
2021-10-12 12:15:34 -0700

    #35 Sponsorship button
    Become a sponsor button links to the sponsorship page

--- src/components/modules/Projects/Project/HelpBuild/HelpBuild.js
+++ src/components/modules/Projects/Project/HelpBuild/HelpBuild.js
@@ -6,27 +6,30 @@ import { Wrapper, ButtonsContainer, Button } from "./StyledHelpBuild";
 const HelpBuild = ({ Color }) => (
   <Section
     bgColor="#494949"
-    Title =" "
+    Title=" "
     Content={
       <Wrapper>
-
         <h3>Help build this project!</h3>
 
-        <p>        
-        Work together with enthusiastic developers (and more, plz someone else write this) 
+        <p>
+          Work together with enthusiastic developers (and more, plz someone else
+          write this)
         </p>
-      
-        <ButtonsContainer>
-          <Button bgColor="#4A9D48">
-            <i className="fas fa-coins"></i> Become a sponsor
 
+        <ButtonsContainer>
+          <Button
+            bgColor="#4A9D48"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "https://devlaunchers.com/c/sponsorship/";
+            }}
+          >
+            <i className="fas fa-coins"></i> Become a sponsor
           </Button>
         </ButtonsContainer>
-        
-        </Wrapper>
+      </Wrapper>
     }
   />
-  
 );
 
 export default withTheme(HelpBuild);
