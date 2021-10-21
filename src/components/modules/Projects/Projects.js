b3dcf38873a39114949f25767bf125ff13a0f222
Hredhya <hredhya3107@gmail.com>
2021-10-21 14:46:25 -0400

    (feat/Hredhya):lazy loading to the projects aggregator page elements

--- src/components/modules/Projects/Projects.js
+++ src/components/modules/Projects/Projects.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useState,useEffect,useRef } from "react";
 import { useRouter } from "next/router";
 import Card from "../../common/Card";
 import { Layout, ProjectContainer } from "./StyledProjects";
@@ -8,30 +8,71 @@ import Fuse from "fuse.js";
 
 const Projects = ({ projects }) => {
   const [searchValue, setSearchValue] = useState('')
+  // Set the state variables for lazy loading
+   const [pageNumber, setPageNumber] = useState(1);
+   const [projsPerPage] = useState(6);
+   const [currentProjects,setCurrentProjects] = useState([])
+   const [lastProjElement, setLastProjElement] = useState(null);
+  
+   const router = useRouter();
+   if (router.isFallback) {
+     return <div>Loading...</div>;
+   }
+  // Added for Project lazy loading
+  const endPageNumber = Math.ceil(projects?.length / projsPerPage)
+  const indexOfLastProj = pageNumber * projsPerPage;
+  const indexOfFirstProj = indexOfLastProj - projsPerPage;
+  const slicedProjects = projects?.slice(indexOfFirstProj, indexOfLastProj);
 
+  // set the current projects to be rendered 
+  useEffect(() => {
+    if((pageNumber <= endPageNumber) && (projects?.length != currentProjects.length)){
+        setCurrentProjects(prevProjs=> [...prevProjs ,...slicedProjects] )
+    }
+  }, [pageNumber])
+
+ // creating an intersection observer
+  const observer = useRef()
+  useEffect(() => {
+  observer.current =new IntersectionObserver(
+      function (entries) {
+        if (entries[0].isIntersecting) {
+          setPageNumber(prevPageNumber => prevPageNumber + 1);
+      }
+    },{ threshold: 1 })
+  }, [])
+
+  // observe the last project card element rendered
+  useEffect(() => {
+    const currentElement = lastProjElement;
+    const currentObserver = observer.current;
+    if (currentElement) {
+       currentObserver.observe(currentElement);
+    }
+    return () => {
+     // to unobserve the previous last-project card   
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [lastProjElement]);
+
+  // for project search functionality
   const options = {
     includeScore: true,
     keys: ["keywords.keyword"],
     threshold: 0.3,
     ignoreFieldNorm: true
   };
-
   const fuse = new Fuse(projects, options);
   const searchResult = fuse.search(searchValue).map(({ item }) => item);
-
   const searchProject = (searchQuery) => {
     setSearchValue(searchQuery)
   };
 
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
-
-  const items = searchValue ? searchResult : projects
-
-  return (
+  const items = searchValue ? searchResult : currentProjects
+  
+   return (
     <div
       style={{
         width: "90%",
@@ -59,33 +100,66 @@ const Projects = ({ projects }) => {
         while learning valuable skills and meeting awesome people!
       </div>
       <Layout>
-        {items.map((project, i) => (
-          <ProjectContainer key={i}>
-            <Card
-              isLinkingInside
-              style={{ margin: 0, width: "100%", height: "100%" }}
-              cardData={{
-                id: project.id,
-                title: project.title,
-                secondaryText: `Commitment level: ${project.commitmentLevel}`,
-                tags: project.keywords.map(({ keyword }) => keyword),
-                description: project.catchPhrase,
-                href: project.slug,
-                imageSrc: project.heroImage.url,
-                actions: (
-                  <>
-                    <Link href={`projects/${project.slug}` || ""} passHref>
-                      <a>LEARN MORE</a>
-                    </Link>
-                    <Link href="support-us" passHref>
-                      <a>DONATE</a>
-                    </Link>
-                  </>
-                ),
-              }}
-            />
-          </ProjectContainer>
-        ))}
+        {items.map((project, i) => {
+          // to have reference to the last project card rendered
+          if((items.length === i + 1) && (projects?.length != items.length)){
+            return( 
+              <ProjectContainer key={i} ref={setLastProjElement}>
+              <Card
+                isLinkingInside
+                style={{ margin: 0, width: "100%", height: "100%" }}
+                cardData={{
+                  id: project.id,
+                  title: project.title,
+                  secondaryText: `Commitment level: ${project.commitmentLevel}`,
+                  tags: project.keywords.map(({ keyword }) => keyword),
+                  description: project.catchPhrase,
+                  href: project.slug,
+                  imageSrc: project.heroImage.url,
+                  actions: (
+                    <>
+                      <Link href={`projects/${project.slug}` || ""} passHref>
+                        <a>LEARN MORE</a>
+                      </Link>
+                      <Link href="support-us" passHref>
+                        <a>DONATE</a>
+                      </Link>
+                    </>
+                  ),
+                }}
+              />
+            </ProjectContainer>)
+          } else {
+            return(
+              <ProjectContainer key={i}>
+              <Card
+                isLinkingInside
+                style={{ margin: 0, width: "100%", height: "100%" }}
+                cardData={{
+                  id: project.id,
+                  title: project.title,
+                  secondaryText: `Commitment level: ${project.commitmentLevel}`,
+                  tags: project.keywords.map(({ keyword }) => keyword),
+                  description: project.catchPhrase,
+                  href: project.slug,
+                  imageSrc: project.heroImage.url,
+                  actions: (
+                    <>
+                      <Link href={`projects/${project.slug}` || ""} passHref>
+                        <a>LEARN MORE</a>
+                      </Link>
+                      <Link href="support-us" passHref>
+                        <a>DONATE</a>
+                      </Link>
+                    </>
+                  ),
+                }}
+              />
+            </ProjectContainer>
+            )
+          }
+          }
+          )}
       </Layout>
     </div>
   );
