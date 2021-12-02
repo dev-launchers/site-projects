fa1cb240df0f14b6b650c0060ea1a0e0ff08f56f
Folarin <folarin.tunji98@gmail.com>
2021-12-02 10:54:46 +0100

    feat: Button Component converted to .ts

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,13 +1,13 @@
 import styled, { css } from "styled-components";
 
 const shadeColor = (color, percent) => {
-  let R = parseInt(color.substring(1, 3), 16);
-  let G = parseInt(color.substring(3, 5), 16);
-  let B = parseInt(color.substring(5, 7), 16);
+  let R: number = parseInt(color.substring(1, 3), 16);
+  let G: number = parseInt(color.substring(3, 5), 16);
+  let B: number = parseInt(color.substring(5, 7), 16);
 
-  R = parseInt((R * (100 + percent)).toString() / 100, 10);
-  G = parseInt((G * (100 + percent)).toString() / 100, 10);
-  B = parseInt((B * (100 + percent)).toString() / 100, 10);
+  R = parseInt(((R * (100 + percent)) / 100).toString() , 10);
+  G = parseInt(((G * (100 + percent)) / 100).toString() , 10);
+  B = parseInt(((B * (100 + percent)) / 100).toString() , 10);
 
   R = R < 255 ? R : 255;
   G = G < 255 ? G : 255;
